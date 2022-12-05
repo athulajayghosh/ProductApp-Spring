c2d59918b8e702d344058a46d8eb70a133003de4
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-12-05 17:19:31 +0530

    pages linked commit

--- src/main/java/com/example/Productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/Productapp_backend/controller/ProductController.java
@@ -1,8 +1,36 @@
 package com.example.Productapp_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class ProductController {
+    @PostMapping("/")
+    public String Homepage(){
+        return "Homepage";
+    }
+
+    @PostMapping("/add")
+    public String add(){
+        return "Add product Page";
+    }
+
+    @PostMapping("/search")
+    public String search(){
+        return "Search product page";
+    }
+    @PostMapping("/edit")
+    public String edit(){
+        return "Edit product page";
+    }
+    @GetMapping("/viewall")
+    public String viewall(){
+        return "View all product page";
+    }
+    @PostMapping("/delete")
+    public String delete(){
+        return "delete product page";
+    }
 }
