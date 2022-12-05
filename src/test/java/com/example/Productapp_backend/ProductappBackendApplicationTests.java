32e996c59f5c58a8a321271602bdd69854781169
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-12-05 17:15:56 +0530

    first commit

---
+++ src/test/java/com/example/Productapp_backend/ProductappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Productapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
